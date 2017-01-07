44325c83322dc1ace88d50cdce407df6e34f3068
Matt Solnit <msolnit@soasta.com>
2017-01-06 21:27:36 -0800

    Initial commit.

---
+++ truncate_dynamo_table.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script deletes all the rows from a DynamoDB table.
+
+OPTIONS:
+   -h      Show this message
+   -t      Table name
+   -k      Key column name
+   -p      AWS CLI profile name (optional)
+   -r      Region (optional if your AWS CLI has a default region)
+EOF
+}
+
+while getopts "t:k:p:r:h" OPTION
+do
+  case $OPTION in
+    h)
+      usage
+      exit 1
+      ;;
+    t)
+      TABLE=$OPTARG
+      ;;
+    k)
+      KEY_COLUMN=$OPTARG
+      ;;
+    r)
+      REGION=$OPTARG
+      ;;
+    p)
+      PROFILE=$OPTARG
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$TABLE" ]; then
+  echo "Table name is required (use the -t parameter)."
+  exit 1
+fi
+if [ -z "$KEY_COLUMN" ]; then
+  echo "Key column name is required (use the -k parameter)."
+  exit 1
+fi
+
+echo "Truncating table $TABLE in region $REGION..."
+
+if [ -n "$PROFILE" ]; then
+  AWS_CLI_ARGS="$AWS_CLI_ARGS --profile $PROFILE"
+fi
+if [ -n "$REGION" ]; then
+  AWS_CLI_ARGS="$AWS_CLI_ARGS --region $REGION"
+fi
+
+KEY_JSON=$(aws $AWS_CLI_ARGS dynamodb scan --table-name $TABLE)
+if [ $? -ne 0 ]; then
+  echo "Table scan failed."
+  exit 1
+fi
+
+KEY_LIST=$(echo "$KEY_JSON" | jq ".Items[].$KEY_COLUMN.S" | sed 's/\"//g')
+if [ -z "$KEY_LIST" ]; then
+  echo "Table is empty."
+  exit 0
+fi
+
+KEY_COUNT=$(echo "$KEY_LIST" | wc -l)
+echo "Deleting $KEY_COUNT key(s)."
+
+echo "$KEY_LIST" | xargs -IID aws $AWS_CLI_ARGS dynamodb delete-item --table-name $TABLE --key "{ \"$KEY_COLUMN\": { \"S\": \"ID\" } }"
